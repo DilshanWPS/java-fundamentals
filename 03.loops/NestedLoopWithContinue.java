9cf619335add1fb8e84660b2e6b4af1fa754a7dc
DilshanWPS <sd20020824@gmail.com>
2026-02-16 10:46:24 +0530

    feat: add NestedLoopWithContinue demo showing continue statement

---
+++ 03.loops/NestedLoopWithContinue.java
@@ -0,0 +1,15 @@
+class NestedLoopWithContinue{
+    public static void main(String args[]){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                if(j==2){
+                    continue;
+                }
+                System.out.println(j);
+            }
+            System.out.println(i);
+            System.out.println();
+        }
+
+    }
+}
