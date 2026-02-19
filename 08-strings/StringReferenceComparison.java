3750c9e80a004c2d59f09236a25d2a84b9c4e0e6
DilshanWPS <sd20020824@gmail.com>
2026-02-19 10:48:37 +0530

    feat: add string reference comparison example

---
+++ 08-strings/StringReferenceComparison.java
@@ -0,0 +1,13 @@
+class StringReferenceComparison{
+    public static void main(String args[]){
+        String name1="nimal";
+        String name2="nimal";
+
+        if(name1==name2){
+            System.out.println("same");
+        }else{
+            System.out.println("not same");
+        }
+
+    }
+}
