ae91244b6bfeef4c0b6315bc075209e570513e00
DilshanWPS <sd20020824@gmail.com>
2026-03-03 21:48:45 +0530

    refactor: improve switch-case structure and Scanner usage

--- 09-practise-programs/programme1.java
+++ 09-practise-programs/programme1.java
@@ -13,68 +13,84 @@ class programme1{
         String schoolName=scanner.nextLine();
         printLine();
 
-        System.out.println("Select your role..");
-        System.out.println("1.Principal");
-        System.out.println("2.Teacher");
-        System.out.println("3.Student");
+        boolean flag1=true;
+        String role="";
+        String roleName="";
+
+        while(flag1){
+            System.out.println("Select your role..");
+            System.out.println("1.Principal");
+            System.out.println("2.Teacher");
+            System.out.println("3.Student");
+            printLine();
+
+            System.out.print("Enter your role number : ");
+            String roleNumber=scanner.next();
+            printLine();
+
+
+            switch(roleNumber){
+                case "1":
+                    role="principal";
+                    roleName=getFullName(scanner);
+                    flag1=false;
+                    break;
+
+                case "2":
+                    role="teacher";
+                    roleName=getFullName(scanner);
+                    flag1=false;
+                    break;
+
+                case "3":
+                    role="student";
+                    roleName=getFullName(scanner);
+                    flag1=false;
+                    break;
+
+                default:
+                    System.out.println("Invalid number,please try again..");
+                
+            }  
+        }
+
+        System.out.println("-------------------- "+ role + " dashboard --------------------");
         printLine();
-
-        System.out.print("Enter your role number : ");
-        String roleNumber=scanner.next();
+        System.out.println("welcome "+ roleName +" for the student management system of "+schoolName);
         printLine();
-
-        switch(roleNumber){
-            case "1":
-                String role="principal";
-                String roleName=getFullName();
-                System.out.println("-------------------- "+ role + " dasboard --------------------");
-                printLine();
-                System.out.println("welcome "+ roleName +" for the student management system of "+schoolName);
-                printLine();
-
-                break;
-            case "2":
-                break;
-            case "3":
-                break;
-            default:
-            
-        }  
-
     }
 
     public static void printLine(){
         System.out.println();
     }
 
-    public static String[] getName(){
-        Scanner scanner1=new Scanner(System.in);
+    public static String[] getName(Scanner scanner){
         String[] names=new String[3];
 
-        boolean flag=true;
+        boolean flag2=true;
 
-        while(flag){
+        while(flag2){
 
-            System.out.println("select your suraname");
+            System.out.println("select your title");
             System.out.println("1.Mr");
             System.out.println("2.Mrs");
             System.out.println("3.Ms");
             printLine();
             System.out.print("Enter your number : ");
-            String surname=scanner1.next();
+            String surname=scanner.next();
             
             switch(surname){
                 case "1":
                     names[0]="Mr";
-                    flag=false;
+                    flag2=false;
                     break;
                 case "2":
                     names[0]="Mrs";
-                    flag=false;
+                    flag2=false;
                     break;
                 case "3":
                     names[0]="Ms";
-                    flag=false;
+                    flag2=false;
                     break;
                 default:
                     System.out.println("Invalid entry.please try again");
@@ -85,18 +101,18 @@ class programme1{
         printLine();
 
         System.out.print("Enter your first name : ");
-        names[1]=scanner1.next();
+        names[1]=scanner.next();
         printLine();
 
         System.out.print("Enter your last name : ");
-        names[2]=scanner1.next();
+        names[2]=scanner.next();
         printLine();
 
         return names;
     }
 
-    public static String getFullName(){
-        String[] fullNameArray=getName();
+    public static String getFullName(Scanner scanner){
+        String[] fullNameArray=getName(scanner);
         String fullName="";
 
         for(int i=0;i<3;i++){
@@ -104,7 +120,6 @@ class programme1{
         }
 
         return fullName;
-
     }
 
 }
